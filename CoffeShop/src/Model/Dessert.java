dd8fd488b079d5dcf8200499bfbce203b988b9c5
ahmad <ahmad.jifry@gmail.com>
2019-02-07 12:36:26 +0000

    add dessert class

---
+++ CoffeShop/src/Model/Dessert.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Dessert extends Item {
+	
+	
+
+	public Dessert() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String createID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
