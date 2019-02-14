3162858246d33b91bde2ac9bc59a8dc3f7137cd2
ahmad <ahmad.jifry@gmail.com>
2019-02-14 16:12:35 +0000

    all model class updated, working on load orders

---
+++ CoffeShop/src/Model/Customer.java
@@ -0,0 +1,19 @@
+package Model;
+
+/**
+ * @author ALI AHMAD A ALASMARE 
+ *
+ */
+
+public class Customer {
+	
+	String name ;
+	String id;
+
+	public Customer(String id , String name) {
+		this.name= name;
+		this.id =  id;
+			
+	}
+
+}
--- CoffeShop/src/Model/Order.java
+++ CoffeShop/src/Model/Order.java
@@ -1,9 +1,39 @@
 package Model;
 
-public class Order {
 
+import java.util.Date;
+import java.util.TreeMap;
+
+/**
+ * @author ALI AHMAD A ALASMARE 
+ *
+ */
+
+public class Order {
+	String id ;
+	Date d = new Date();
+	Customer customer;
+	TreeMap<Item,Integer> items ;
+	Boolean preperedFlag = false;
+	Double totalCost ;
+	
 	public Order() {
-		// TODO Auto-generated constructor stub
+		
+		d.setTime(System.currentTimeMillis());
+		
 	}
+	
+	public void addItem (Item item , int count){
+		
+	}
+
+	public Boolean getPreperedFlag() {
+		return preperedFlag;
+	}
+
+	public void setPreperedFlag(Boolean preperedFlag) {
+		this.preperedFlag = preperedFlag;
+	}
+	
 
 }
